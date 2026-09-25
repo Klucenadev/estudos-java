15b9cc6c29e557f701ef82d2ccc0fef10f0ddf19
lucena <mkaio2069@gmail.com>
2026-09-25 16:10:32 -0300

    start maratonajava

---
+++ src/aula01/kaiodev/maratonajava/aula01_comentarios.java
@@ -0,0 +1,20 @@
+package aula01.kaiodev.maratonajava;
+
+public class aula01_comentarios {
+    //comentarios não são muitos ultilizados, entao não encha seu projeto de comentarios
+    //comentario de uma linha
+    /*
+    comentario de
+    multiplas
+    linas
+     */
+//o comentario a baixo faz assim: /** e aperte enter
+    /**
+     *
+     * isto é um comentarios <br> java
+     */
+     public static void main(String[] args){
+        System.out.println("vtmnc");
+
+    }
+}
---
+++ src/aula01/kaiodev/maratonajava/aula02_tiposprimitivosprt1.java
@@ -0,0 +1,11 @@
+package aula01.kaiodev.maratonajava;
+
+
+public class aula02_tiposprimitivosprt1 {
+    static void main(String[] args) {
+        //temos alguns tipos de variaveis: int,float,duble,char,byte,short,boolean.
+        int age = 19;
+        // O + é usado para concatenar
+        System.out.println("sua idade é " + age + " anos");
+    }
+}
